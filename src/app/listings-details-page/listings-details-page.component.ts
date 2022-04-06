d69deb497747c9d6e0f6916a0a10770313310a10
publio.carreiro <publio.carreiro@siemens.com>
2022-04-06 12:40:56 +0100

    detaillist and strict mode for property

--- src/app/listings-details-page/listings-details-page.component.ts
+++ src/app/listings-details-page/listings-details-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { fakeListings } from '../fake-data';
+import { Listing } from '../types';
 
 @Component({
   selector: 'app-listings-details-page',
@@ -6,10 +9,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listings-details-page.component.css']
 })
 export class ListingsDetailsPageComponent implements OnInit {
-
-  constructor() { }
+  listing: Listing;
+  
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.listing = fakeListings.find(listing => listing.id === id);
   }
 
 }
