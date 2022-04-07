65df79db7a84077c1815ed1cf04c203719d9fa96
publio.carreiro <publio.carreiro@siemens.com>
2022-04-07 11:06:20 +0100

    done

--- src/app/my-listings-page/my-listings-page.component.ts
+++ src/app/my-listings-page/my-listings-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { fakeListings, fakeMyListings } from '../fake-data';
+import { Listing } from '../types';
 
 @Component({
   selector: 'app-my-listings-page',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-listings-page.component.css']
 })
 export class MyListingsPageComponent implements OnInit {
+  listings: Listing[] = [];
+
 
   constructor() { }
 
   ngOnInit(): void {
+    this.listings = fakeListings;
+  }
+
+  onDeleteClicked(listingId: string): void{
+    alert(`Deleteing your listing with id ${listingId}!`);
   }
 
 }
