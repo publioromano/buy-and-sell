672d2a3ba92601f71512a3b013ae9a41d764b2e5
publio.carreiro <publio.carreiro@siemens.com>
2022-04-07 14:19:47 +0100

    done my first generic component.

--- src/app/listing-data-form/listing-data-form.component.ts
+++ src/app/listing-data-form/listing-data-form.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
+import { Listing } from '../types';
 
 @Component({
   selector: 'app-listing-data-form',
@@ -7,21 +8,31 @@ import { Router } from '@angular/router';
   styleUrls: ['./listing-data-form.component.css']
 })
 export class ListingDataFormComponent implements OnInit {
-
+  @Input() buttonText;
+  @Input() currentName = '';
+  @Input() currentDescription = '';
+  @Input() currentPrice = '';
   name: string = '';
   description: string = '';
   price: string = '';
+  @Output() onSubmit = new EventEmitter<Listing>();
 
   constructor(
     private router: Router
   ) { }
 
   ngOnInit(): void {
+    this.name = this.currentName;
+    this.description = this.currentDescription;
+    this.price = this.currentPrice;
   }
 
-  onSubmit(): void{
-    alert('Creating a new listing...');
-    this.router.navigateByUrl('/my-listings')
+  onButtonClicked(): void{
+    this.onSubmit.emit({
+      id : null,
+      name: this.name,
+      description: this.description,
+      price: Number(this.price)
+    });
   }
-
 }
